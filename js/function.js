d29e86d00b04b79c82a68cc6053155c4bc90ec26
NikoShoo <NikoWork1@ya.ru>
2025-02-19 19:14:18 +0400

    Выполняет дз№2

---
+++ js/function.js
@@ -0,0 +1,24 @@
+function checkStringLength(string, maxSymbols) {
+  if (string.length <= maxSymbols) {
+    return true;
+  }
+  return false;
+}
+
+checkStringLength('проверяемая строка', 20);
+checkStringLength('проверяемая строка', 18);
+checkStringLength('проверяемая строка', 10);
+
+
+const isPalindrome = (string) => {
+
+  string = string.replaceAll(' ', '').toLowerCase();
+
+  let invertedLine = '';
+
+  for (let i = string.lenth - 1; i >= 0; i--) {
+    invertedLine += string[i];
+  }
+
+  return string === invertedLine;
+};
