51ad89dabaa1ca0012004998f2fe3a08d6a66e3e
NikoShoo <NikoWork1@ya.ru>
2025-06-26 17:05:50 +0400

    Решает задачу "Делу — время"

--- js/function.js
+++ js/function.js
@@ -21,3 +21,23 @@ const isPalindrome = (string) => {
 isPalindrome('проверяемая строка');
 
 
+const MINUTES_PER_HOUR = 60;
+
+const getMinutes = (time) => {
+  const [h, m] = time.split(':');
+  return h * MINUTES_PER_HOUR + +m;
+};
+
+const checkMeeting = (startDay, endDay, startMeeting, duringMeeting) => {
+  const startDayMinutes = getMinutes(startDay);
+  const endDayMinutes = getMinutes(endDay);
+  const startMeetingMinutes = getMinutes(startMeeting);
+  const endMeetingMinutes = startMeetingMinutes + duringMeeting;
+  return startDayMinutes <= startMeetingMinutes && endMeetingMinutes <= endDayMinutes;
+};
+
+console.log(checkMeeting('08:00', '17:30', '14:00', 90));
+console.log(checkMeeting('8:0', '10:0', '8:0', 120));
+console.log(checkMeeting('08:00', '14:30', '14:00', 90));
+console.log(checkMeeting('14:00', '17:30', '08:0', 90));
+console.log(checkMeeting('8:00', '17:30', '08:00', 900));
